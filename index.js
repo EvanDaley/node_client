82cae46f4a8cc811139f34712ff771affca2a26a
Evan Daley <daley.evan@gmail.com>
2019-10-29 20:09:24 -0700

    Work in progress

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var moodle_client = require("moodle-client");
+var winston = require("winston");
+
+var logger = winston.createLogger({
+    level: "debug",
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
 
 moodle_client.init({
-    wwwroot: "http://localhost/moodle/",
-    token: "d457b5e5b0cc31c05ccf38628e4dfc14"
-
+    wwwroot: "http://localhost/",
+    // token: "d457b5e5b0cc31c05ccf38628e4dfc14"
+    username: "admin",
+    password: "MoodlePassword1!",
+    logger: logger
 }).then(function(client) {
     return do_something(client);
 
@@ -11,3 +22,12 @@ moodle_client.init({
     console.log("Unable to initialize the client: " + err);
 });
 
+function do_something(client) {
+    return client.call({
+        wsfunction: "core_webservice_get_site_info",
+
+    }).then(function(info) {
+        console.log("Hello %s, welcome to %s", info.fullname, info.sitename);
+        return;
+    });
+}
