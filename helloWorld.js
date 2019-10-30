524802caba09255dc7f654ce399ccc03c29905a4
Evan Daley <daley.evan@gmail.com>
2019-10-29 22:26:16 -0700

    Work in progress

---
+++ helloWorld.js
@@ -0,0 +1,36 @@
+// FOR REFERENCE
+// THIS SCRIPT HITS THE HELLO WORLD WEBSERVICE
+// Run with 'npm run hello'
+
+var moodle_client = require("moodle-client");
+var winston = require("winston");
+
+var logger = winston.createLogger({
+    level: "debug",
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+moodle_client.init({
+    wwwroot: "http://localhost/",
+    // token: "d457b5e5b0cc31c05ccf38628e4dfc14"
+    username: "admin",
+    password: "MoodlePassword1!",
+    logger: logger,
+    service: 'hello_world'
+}).then(function(client) {
+    return hello_world(client);
+}).catch(function(err) {
+    console.log("Unable to initialize the client: " + err);
+});
+
+function hello_world(client) {
+    return client.call({
+        wsfunction: "local_wstemplate_hello_world",
+    }).then(function(info) {
+        console.log(info);
+        return;
+    });
+}
---
+++ siteInfo.js
@@ -0,0 +1,36 @@
+// FOR REFERENCE
+// THIS SCRIPT HITS THE SITE INFO WEBSERVICE
+// Run with 'npm run info'
+
+var moodle_client = require("moodle-client");
+var winston = require("winston");
+
+var logger = winston.createLogger({
+    level: "debug",
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+moodle_client.init({
+    wwwroot: "http://localhost/",
+    // token: "d457b5e5b0cc31c05ccf38628e4dfc14"
+    username: "admin",
+    password: "MoodlePassword1!",
+    logger: logger,
+}).then(function(client) {
+    return get_site_info(client);
+}).catch(function(err) {
+    console.log("Unable to initialize the client: " + err);
+});
+
+function get_site_info(client) {
+    return client.call({
+        wsfunction: "core_webservice_get_site_info",
+    }).then(function(info) {
+        console.log(info);
+        return;
+    });
+}
+
